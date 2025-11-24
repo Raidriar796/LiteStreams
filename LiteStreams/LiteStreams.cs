e77624898519b903b0c43c05ec9e195584040af7
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-11-24 13:58:20 -0800

    Project setup

---
+++ LiteStreams/LiteStreams.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using Elements.Assets;
+using Elements.Core;
+
+namespace LiteStreams;
+
+public class LiteStreams : ResoniteMod
+{
+    public override string Name => "LiteStreams";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/LiteStreams";
+    public static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.LiteStreams");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+}
