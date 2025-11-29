6e80a19f74e203de5eb28ca475c58ae0d7fb2677
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-11-28 17:51:59 -0800

    Prevent race conditions
    Config handling now checks if the world has been focused at least once before updating streams.

--- LiteStreams/ConfigHandling.cs
+++ LiteStreams/ConfigHandling.cs
@@ -11,57 +11,61 @@ public partial class LiteStreams : ResoniteMod
         // iterate over every world
         foreach (World world in Engine.Current.WorldManager.Worlds)
         {
-            world.RunSynchronously(() =>
+            // Only update worlds that you have focused in at least once
+            if (firstFocusList[world] == true)
             {
-                // iterate over every stream on the user
-                foreach (FrooxEngine.Stream stream in world.LocalUser.Streams)
+                world.RunSynchronously(() =>
                 {
-                    // Only change the period of streams if they are implicit streams
-                    if (stream is ImplicitStream implicitStream)
+                    // iterate over every stream on the user
+                    foreach (FrooxEngine.Stream stream in world.LocalUser.Streams)
                     {
-                        // Depending on if the mod was enabled or disabled, double or half the stream period
-                        if (Config!.GetValue(Enable))
+                        // Only change the period of streams if they are implicit streams
+                        if (stream is ImplicitStream implicitStream)
                         {
-                            implicitStream.SetUpdatePeriod(implicitStream.Period * 2, implicitStream.Phase);
-                        }
-                        else
-                        {
-                            implicitStream.SetUpdatePeriod((uint)(implicitStream.Period * 0.5), implicitStream.Phase);
-                        }
-                    }
-                    // Only change the bitrate of the streams if it's the user's voice stream
-                    else if (stream is OpusStream<MonoSample> voiceStream)
-                    {
-                        // See if the stream is a mono opus stream, incase a stream is named
-                        // voice when it's not the user's voice for whatever reason
-                        if (stream.Name == "Voice")
-                        {
-                            // Depending on if the mod was enabled or disabled, set the voice bitrate to the original or the configured bitrate
+                            // Depending on if the mod was enabled or disabled, double or half the stream period
                             if (Config!.GetValue(Enable))
                             {
-                                voiceStream.BitRate.Value = (int)Config!.GetValue(voiceQuality);
+                                implicitStream.SetUpdatePeriod(implicitStream.Period * 2, implicitStream.Phase);
                             }
                             else
                             {
-                                voiceStream.BitRate.Value = (int)VoiceQuality.Default;
+                                implicitStream.SetUpdatePeriod((uint)(implicitStream.Period * 0.5), implicitStream.Phase);
+                            }
+                        }
+                        // Only change the bitrate of the streams if it's the user's voice stream
+                        else if (stream is OpusStream<MonoSample> voiceStream)
+                        {
+                            // See if the stream is a mono opus stream, incase a stream is named
+                            // voice when it's not the user's voice for whatever reason
+                            if (stream.Name == "Voice")
+                            {
+                                // Depending on if the mod was enabled or disabled, set the voice bitrate to the original or the configured bitrate
+                                if (Config!.GetValue(Enable))
+                                {
+                                    voiceStream.BitRate.Value = (int)Config!.GetValue(voiceQuality);
+                                }
+                                else
+                                {
+                                    voiceStream.BitRate.Value = (int)VoiceQuality.Default;
+                                }
+                            }
+                        }
+                        // Only change the minimum volume of the streams if they're stereo opus streams
+                        else if (stream is OpusStream<StereoSample> audioStream)
+                        {
+                            // Depending on if the mod was enabled or disabled, set or unset a minimum volume
+                            if (Config!.GetValue(Enable))
+                            {
+                                audioStream.MinimumVolume.Value = 0.005f;
+                            }
+                            else
+                            {
+                                audioStream.MinimumVolume.Value = 0f;
                             }
                         }
                     }
-                    // Only change the minimum volume of the streams if they're stereo opus streams
-                    else if (stream is OpusStream<StereoSample> audioStream)
-                    {
-                        // Depending on if the mod was enabled or disabled, set or unset a minimum volume
-                        if (Config!.GetValue(Enable))
-                        {
-                            audioStream.MinimumVolume.Value = 0.005f;
-                        }
-                        else
-                        {
-                            audioStream.MinimumVolume.Value = 0f;
-                        }
-                    }
-                }
-            });
+                });
+            }
         }
     }
 
@@ -71,26 +75,30 @@ public partial class LiteStreams : ResoniteMod
         {
             foreach (World world in Engine.Current.WorldManager.Worlds)
             {
-                world.RunSynchronously(() =>
+                // Only update worlds that you have focused in at least once
+                if (firstFocusList[world] == true)
                 {
-                    // Runs on every stream for the local user
-                    foreach (FrooxEngine.Stream stream in world.LocalUser.Streams)
+                    world.RunSynchronously(() =>
                     {
-                        // Check for the user's voice audio stream
-                        if (stream.Name == "Voice")
+                        // Runs on every stream for the local user
+                        foreach (FrooxEngine.Stream stream in world.LocalUser.Streams)
                         {
-                            // See if the stream is a mono opus stream, incase a stream is named
-                            // voice when it's not the user's voice for whatever reason
-                            if (stream is OpusStream<MonoSample> voiceStream)
+                            // Check for the user's voice audio stream
+                            if (stream.Name == "Voice")
                             {
-                                // Reduces the bitrate, default value is 25000
-                                voiceStream.BitRate.Value = (int)Config!.GetValue(voiceQuality);
-                                // Break the loop early, as there is no need to search anymore
-                                break;
+                                // See if the stream is a mono opus stream, incase a stream is named
+                                // voice when it's not the user's voice for whatever reason
+                                if (stream is OpusStream<MonoSample> voiceStream)
+                                {
+                                    // Reduces the bitrate, default value is 25000
+                                    voiceStream.BitRate.Value = (int)Config!.GetValue(voiceQuality);
+                                    // Break the loop early, as there is no need to search anymore
+                                    break;
+                                }
                             }
                         }
-                    }
-                });
+                    });
+                }
             }
         }
     }
